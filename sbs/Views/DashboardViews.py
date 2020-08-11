29d770ff2c15bad692a22cc7f37a86f3d1350500
fatih-berktug <faatihberktug@hotmail.com>
2020-08-11 16:33:08 +0300

    Bildirimler Açılıyor

--- sbs/Views/DashboardViews.py
+++ sbs/Views/DashboardViews.py
@@ -7,6 +7,10 @@ from django.http import JsonResponse
 
 from sbs.models import SportClubUser, SportsClub, Coach, Level, License, Athlete, Person, Judge
 from sbs.services import general_methods
+
+from sbs.models.ReferenceCoach import ReferenceCoach
+from sbs.models.ReferenceReferee import ReferenceReferee
+from sbs.models.PreRegistration import PreRegistration
 # from rest_framework.authtoken.models import Token
 
 
@@ -159,11 +163,20 @@ def return_admin_dashboard(request):
     total_judge = Judge.objects.all().count()
     total_user = User.objects.all().count()
 
+    total_notifications_refere = ReferenceReferee.objects.filter(status=ReferenceReferee.WAITED).count()
+    total_notifications_coach = ReferenceReferee.objects.filter(status=ReferenceCoach.WAITED).count()
+    total_notifications_clup = PreRegistration.objects.filter(status=PreRegistration.WAITED).count()
+    notifications_tatal = total_notifications_refere + total_notifications_coach + total_notifications_clup
+
     return render(request, 'anasayfa/admin.html',
                   {'total_club_user': total_club_user, 'total_club': total_club,
                    'total_athlete': total_athlete, 'total_coachs':total_coachs,'last_athletes':last_athlete,'total_athlete_gender_man':total_athlete_gender_man,
                    'total_athlete_gender_woman':total_athlete_gender_woman,'total_athlate_last_month':total_athlate_last_month,
-                   'total_judge': total_judge, 'total_user': total_user
+                   'total_judge': total_judge, 'total_user': total_user,
+                   'total_notifications_refere': total_notifications_refere,
+                   'total_notifications_coach': total_notifications_coach,
+                   'total_notifications_clup': total_notifications_clup,
+                   'notifications_tatal': notifications_tatal
 
                    })
 
