cb90942d8b7249aaa1ecd278772161e887d890b2
fatih-berktug <faatihberktug@hotmail.com>
2021-01-11 10:07:35 +0300

    Kullanici admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+
+admin.site.site_header = 'Kobiltek Bilisim Kullanici Yönetim Paneli '  # default: "Django Administration"
+admin.site.index_title = 'Sistem Yönetimi'  # default: "Site administration"
+admin.site.site_title = 'Admin'  # default: "Django site admin"
+
+
+
 from sbs.models.CategoryItem import CategoryItem
 from sbs.models.DirectoryCommission import DirectoryCommission
 from sbs.models.DirectoryMember import DirectoryMember
